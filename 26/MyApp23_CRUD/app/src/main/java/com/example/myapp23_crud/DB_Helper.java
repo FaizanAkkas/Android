76856520909455e738e8d928623adb8ef74a9e6c
asp <asp@aptech.com>
2023-02-09 11:05:05 +0500

    09-Feb-2023

---
+++ 26/MyApp23_CRUD/app/src/main/java/com/example/myapp23_crud/DB_Helper.java
@@ -0,0 +1,96 @@
+package com.example.myapp23_crud;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.media.tv.TableRequest;
+
+import androidx.annotation.Nullable;
+import androidx.core.database.sqlite.SQLiteDatabaseKt;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import kotlin.Suppress;
+
+public class DB_Helper extends SQLiteOpenHelper {
+    public  static  final  String DATABASE_NAME = "Info.db";
+    public  static  final  String TABLE_NAME = "Student_Table";
+    public  static  final  String Col_1 = "Std_ID";
+    public  static  final  String Col_2 = "Std_Name";
+    public  static  final  String Col_3 = "Std_Email";
+    public  static  final  String Col_4 = "Std_BatchCode";
+    public DB_Helper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("create table " + TABLE_NAME + " (Std_ID INTEGER PRIMARY KEY AUTOINCREMENT,Std_Name TEXT,Std_Email TEXT,Std_BatchCode TEXT ) ");
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("Drop Table IF EXISTS "+TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+    public  boolean Insert_Record(String name,String email,String bc)
+    {
+        SQLiteDatabase sql_obj = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("Std_Name",name);
+        cv.put(Col_3,email);
+        cv.put(Col_4,bc);
+       long success= sql_obj.insert(TABLE_NAME,null,cv);
+        if(success == -1)
+        {
+            return  false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    //-------------------------------------------------------
+    @SuppressLint("Range")
+    public ArrayList<HashMap<String,String>> Get_Record()
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ArrayList<HashMap<String,String>> userlist = new ArrayList<>();
+        String query = "Select Std_ID,Std_Name,Std_Email,Std_BatchCode FROM "+TABLE_NAME;
+        Cursor cursor =db.rawQuery(query,null);
+        while (cursor.moveToNext())
+        {
+            HashMap<String,String> user = new HashMap<>();
+            user.put("id_std",cursor.getString(cursor.getColumnIndex(Col_1)));
+            user.put("name_std",cursor.getString(cursor.getColumnIndex(Col_2)));
+            user.put("email_std",cursor.getString(cursor.getColumnIndex(Col_3)));
+            user.put("bc_std",cursor.getString(cursor.getColumnIndex(Col_4)));
+            userlist.add(user);
+
+        }
+        return  userlist;
+    }
+//------------------------------------------------ delete record
+    public  boolean Delete_Record(int del_id)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+      return   db.delete(TABLE_NAME,"Std_ID = "+del_id,null) >0;
+
+    }
+    //------------------------------------------------ Update record
+    public  void Update_Record(String up_id,String up_name,String up_email,String up_bc)
+    {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(Col_1,up_id);
+        cv.put(Col_2,up_name);
+        cv.put(Col_3,up_email);
+        cv.put(Col_4,up_bc);
+        String whereclause="Std_ID=?";
+        String whereargs[] = {up_id};
+        db.update(TABLE_NAME,cv,whereclause,whereargs);
+
+    }
+}
---
+++ 26/MyApp23_CRUD/app/src/main/java/com/example/myapp23_crud/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.myapp23_crud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText_id,editText_un,editText_email,editText_bc;
+    Button button_insert,button_view,button_update;
+    DB_Helper db_helper;
+    public  static  final  String DATABASE_NAME = "Info.db";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText_id = findViewById(R.id.txt_id);
+        editText_un = findViewById(R.id.txt_un);
+        editText_email = findViewById(R.id.txt_email);
+        editText_bc = findViewById(R.id.txt_batchcode);
+        button_insert = findViewById(R.id.btn_insert);
+        button_view = findViewById(R.id.btn_view);
+        button_update = findViewById(R.id.btn_update);
+        db_helper = new DB_Helper(MainActivity.this);
+
+        Intent intent = getIntent();
+        if (intent != null)
+        {   editText_id.setText(intent.getStringExtra("Key_sid"));
+            editText_un.setText(intent.getStringExtra("Key_sname"));
+            editText_email.setText(intent.getStringExtra("Key_semail"));
+            editText_bc.setText(intent.getStringExtra("Key_sbc"));
+        }
+
+
+        //------------------------------------------------insertion operation
+        button_insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              boolean check=  db_helper.Insert_Record(editText_un.getText().toString(),editText_email.getText().toString(),editText_bc.getText().toString());
+            if(check)
+            {
+                Toast.makeText(MainActivity.this, "Record Inserted", Toast.LENGTH_SHORT).show();
+
+            }
+            else
+            {
+                Toast.makeText(MainActivity.this, "Record not Inserted", Toast.LENGTH_SHORT).show();
+
+            }
+            }
+        });
+        button_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ShowActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        button_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db_helper.Update_Record(editText_id.getText().toString(),editText_un.getText().toString(),
+                                        editText_email.getText().toString(),editText_bc.getText().toString());
+                Toast.makeText(MainActivity.this, "Record Updated", Toast.LENGTH_SHORT).show();
+                editText_id.setText("");
+                editText_un.setText("");
+                editText_email.setText("");
+                editText_bc.setText("");
+            }
+        });
+    }
+}
